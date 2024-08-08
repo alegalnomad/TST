d441cd6a79bd21e9f3d4e02cf7a275b5215c5ddd
alegalnomad <anand.raghav93@gmail.com>
2024-08-08 16:41:13 +0100

    Update app.py

--- app.py
+++ app.py
@@ -71,4 +71,4 @@ def predict():
             return jsonify({'error': f'Error during prediction: {str(e)}'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080 ,debug=True)
